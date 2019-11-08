e26092d64911cd0bf0a40d0fdf7746a88c1f0bc5
José Osorio <joseosorio001@outlook.com>
2019-11-08 14:54:10 -0600

    Testing urls

--- invitations/landing/models.py
+++ invitations/landing/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Participante(models.Model):
+
+    def __str__(self):
+        pass
+
+    class Meta:
+        verbose_name = 'Participante'
+        verbose_name_plural = 'Participantes'
