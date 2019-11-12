056b19a61f867412f35a07ef2f7983f809e1a827
José Osorio <joseosorio001@outlook.com>
2019-11-12 16:08:09 -0600

    Custom admin site

--- invitations/landing/models.py
+++ invitations/landing/models.py
@@ -11,18 +11,18 @@ def createHash():
 
 # Create your models here.
 class Participante(models.Model):
-    codigo_participante = models.TextField(default=createHash, unique=True, verbose_name="Código de participante")
+    codigo_participante = models.TextField(default=createHash, unique=True, verbose_name="Código")
     nombre = models.CharField(max_length=255, verbose_name="Nombre del participante")
     empresa = models.CharField(max_length=255, verbose_name="Empresa (Razón social)")
     cargo = models.CharField(max_length=100, verbose_name="Cargo")
     email = models.EmailField(unique=True, verbose_name="email")
-    telefono = models.CharField(max_length=20, verbose_name="Número de teléfono", blank=True, null=True)
+    telefono = models.CharField(max_length=20, verbose_name="Teléfono", blank=True, null=True)
     f_name = models.CharField(max_length=255, verbose_name="Facturar a nombre de")
     direccion = models.CharField(max_length=255, verbose_name="Dirección")
     giro = models.CharField(max_length=255, verbose_name="Giro", blank=True, null=True)
     nit = models.CharField(max_length=17, verbose_name="NIT")
     nrc = models.CharField(max_length=10, verbose_name="NRC")
-    orden_compra = models.CharField(max_length=255, verbose_name="Orden de compra No.", blank=True, null=True)
+    orden_compra = models.CharField(max_length=255, verbose_name="Orden de compra", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de registro')
 
     def recently_created(self):
@@ -42,11 +42,11 @@ class Participante(models.Model):
     
 class Asistente(models.Model):
     registered_at = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de registro')
-    participante = models.OneToOneField(Participante, verbose_name="participante", on_delete=models.DO_NOTHING, unique=True)
+    participante = models.OneToOneField(Participante, verbose_name="Asistió", on_delete=models.DO_NOTHING, unique=True)
     
     def __str__(self):
         return self.participante.nombre
 
     class Meta:
-        verbose_name = 'Asistente'
-        verbose_name_plural = 'Asistentes'
+        verbose_name = 'Asistencia'
+        verbose_name_plural = 'Asistencias'
