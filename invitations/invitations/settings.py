002067910ba9540ae6ffaaf0f0e31d7b7d68306f
José Osorio <joseosorio001@outlook.com>
2019-11-12 17:02:19 -0600

    Debug False

--- invitations/invitations/settings.py
+++ invitations/invitations/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['bccelsalvador.treecloud.co', '127.0.0.1', 'localhost']
 
