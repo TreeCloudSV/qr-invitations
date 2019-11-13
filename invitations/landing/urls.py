9fa05cdae0ff4419feffb9aab83862975ed3f40a
José Osorio <joseosorio001@outlook.com>
2019-11-12 19:00:48 -0600

    Harcoded some data for deliver at time

--- invitations/landing/urls.py
+++ invitations/landing/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', RegisterView.as_view(), name='index'),
     path('validate/<codigo_participante>/', validate, name='validate-attendance'),
     path('finish/<codigo>/', finish, name="finish"),
+    path('prueba/', prueba, name="prueba"), 
 ]
