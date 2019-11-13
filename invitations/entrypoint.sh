150959d4722c41a4b9108ee53b48c5f3005e85ea
Alejandro Vásquez <jose5m45@gmail.com>
2019-11-13 03:02:59 +0000

    .

--- invitations/entrypoint.sh
+++ invitations/entrypoint.sh
@@ -2,5 +2,5 @@
 export $(egrep  -v '^#'  /run/secrets/* | xargs)
 python manage.py migrate
 python manage.py collectstatic  --no-input
-python manage.py runserver 0.0.0.0:8000
+gunicorn invitations.wsgi:application --bind 0.0.0.0:8000 --workers 3
 
