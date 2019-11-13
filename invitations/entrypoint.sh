345b763159d69e6e462395cad76eb80eebaa7085
Alejandro Vásquez <jose5m45@gmail.com>
2019-11-13 04:08:36 +0000

    algo hace xd

--- invitations/entrypoint.sh
+++ invitations/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 export $(egrep  -v '^#'  /run/secrets/* | xargs)
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+python manage.py collectstatic --no-input && python manage.py makemigrations && python manage.py migrate && gunicorn --workers=3 invitations.wsgi -b 0.0.0.0:8000
+
--- invitations/invitations/settings.py
+++ invitations/invitations/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = '/app/static'
 
 # Email config
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
